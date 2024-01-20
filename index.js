0de712d950280db426f8501d83516e17986eebe4
sathish3110 <sathish.kumar@harnstech.com>
2024-01-20 14:18:50 +0530

    server setup

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 const port = 3000;
-app.get("api/start", (req, res) => {
+app.get("/api/start", (req, res) => {
   res.json({ msg: "Welcome to my server" });
 });
 app.listen(port, () => console.log(`App is listening to the ${port}`));
